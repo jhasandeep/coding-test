3bbd79bb495bd1dbe776027df99b6dd8b4e31068
jhasandeep <121882218+jhasandeep@users.noreply.github.com>
2023-01-26 11:47:07 +0530

    Update longest_common_substrin.py

--- longest_common_substrin.py
+++ longest_common_substrin.py
@@ -1,1 +1,22 @@
+# Write a Python program that finds the longest common substring between two strings.
 
+A = input()
+B = input()
+
+if len(A) > len(B) :
+    bigger = len(A)
+else:
+    bigger = len(B)
+
+count = 0
+for i in range(bigger):
+    
+        
+    if A[i] == B[i]: 
+        count +=1 
+    else: 
+        count +=0
+        break
+        
+            
+print(count)
